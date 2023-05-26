793e5a7c5b6b45c661fd599de237e0027a40baa6
emtech-user <emtech-user@192.168.128.103>
2023-05-26 13:26:09 +0900

    DAOno追加;satp

--- Amaats/src/main/java/model/entity/SweetsBean.java
+++ Amaats/src/main/java/model/entity/SweetsBean.java
@@ -3,35 +3,55 @@ package model.entity;
 import java.io.Serializable;
 
 public class SweetsBean implements Serializable {
-	
+
 	private String sweets_id;
-	private String name;
-	private int price;
-	
+	private String sweets_name;
+	private int sweets_value;
+	private String sweets_genre;
+	private String shop_id;
+
 	/* コンストラクタ */
 	public SweetsBean() {
-		
+
 	}
-	
-	/* アクセサメソッド */
+
 	public String getSweets_id() {
 		return sweets_id;
 	}
+
 	public void setSweets_id(String sweets_id) {
 		this.sweets_id = sweets_id;
 	}
-	public String getName() {
-		return name;
+
+	public String getSweets_name() {
+		return sweets_name;
 	}
-	public void setName(String name) {
-		this.name = name;
+
+	public void setSweets_name(String sweets_name) {
+		this.sweets_name = sweets_name;
 	}
-	public int getPrice() {
-		return price;
+
+	public int getSweets_value() {
+		return sweets_value;
 	}
-	public void setPrice(int price) {
-		this.price = price;
+
+	public void setSweets_value(int sweets_value) {
+		this.sweets_value = sweets_value;
 	}
-	
-	
+
+	public String getSweets_genre() {
+		return sweets_genre;
+	}
+
+	public void setSweets_genre(String sweets_genre) {
+		this.sweets_genre = sweets_genre;
+	}
+
+	public String getShop_id() {
+		return shop_id;
+	}
+
+	public void setShop_id(String shop_id) {
+		this.shop_id = shop_id;
+	}
 }
