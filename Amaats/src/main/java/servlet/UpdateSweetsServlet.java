23a57c5531126916ea35831afb5c7f0dffd19e5b
emtech-user <emtech-user@192.168.128.103>
2023-05-26 14:20:00 +0900

    UpdateServlet:sato

---
+++ Amaats/src/main/java/servlet/UpdateSweetsServlet.java
@@ -0,0 +1,70 @@
+package servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.dao.SweetsDAO;
+import model.entity.SweetsBean;
+/**
+ * スイーツの変更制御
+ * @author emtech-user
+ *
+ */
+@WebServlet("/update-sweets-servlet")
+public class UpdateSweetsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateSweetsServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// セッションオブジェクトの取得
+		HttpSession session = request.getSession();
+
+		// セッションスコープからの属性値の取得
+		SweetsBean sweets = (SweetsBean) session.getAttribute("sweets");
+
+		// DAOの生成
+		SweetsDAO dao = new SweetsDAO();
+
+		int processingNumber = 0; //処理件数
+
+		try {
+			// DAOの利用
+			processingNumber = dao.update(sweets);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+
+		// リクエストスコープへの属性の設定
+		request.setAttribute("processingNumber", processingNumber);
+
+		// リクエストの転送
+		RequestDispatcher rd = request.getRequestDispatcher("sweets-update-result.jsp");
+		rd.forward(request, response);
+	}
+
+}
