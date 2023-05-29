5e198e3284f10ce89ce541ad4331282dbde75a98
emtech-user <emtech-user@192.168.128.103>
2023-05-29 17:47:57 +0900

    レビュー機能：sato

---
+++ Amaats/src/main/java/model/dao/ReviewDAO.java
@@ -0,0 +1,76 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entity.ReviewBean;
+
+
+public class ReviewDAO {
+	/**
+	 * 口コミ
+	 */
+	public List<ReviewBean> getReviewList(int sweetsId)
+			throws SQLException, ClassNotFoundException {
+
+		List<ReviewBean> reviewList = new ArrayList<ReviewBean>();
+		String sql ="SELECT * FROM t_review WHERE sweets_id =?";
+
+		// データベースへの接続の取得、Statementの取得、SQLステートメントの実行
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)){
+
+
+
+			// プレースホルダへの値の設定
+			pstmt.setInt(1, sweetsId);
+			ResultSet res =pstmt.executeQuery();
+
+
+			while(res.next()) {
+
+				ReviewBean review = new ReviewBean();
+				review.setReview_id(res.getInt("review_id"));
+				review.setReview_text(res.getString("review_text"));
+				review.setUser_id(res.getString("user_id"));
+				review.setSweets_id(res.getInt("sweets_id"));
+
+				reviewList.add(review);
+
+			}
+			return reviewList;
+		}
+	}
+
+	/*口コミ登録機能*/
+	public int insertReview(ReviewBean review) throws SQLException, ClassNotFoundException {
+		int count = 0; //処理件数
+		//商品の追加
+		// データベースへの接続の取得、PreparedStatementの取得
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement("INSERT INTO t_review  VALUE(?, ?, ?, ?)")) {
+
+			// DTOからのデータの取り出し
+			int review_id= review.getReview_id();
+			String review_text = review.getReview_text();
+			String  user_id = review.getUser_id();
+			int sweets_id = review.getSweets_id();
+
+			// プレースホルダへの値の設定
+			pstmt.setInt(1, review_id);
+			pstmt.setString(2, review_text);
+			pstmt.setString(3, user_id);
+			pstmt.setInt(4, sweets_id);
+
+			// SQLステートメントの実行
+			count = pstmt.executeUpdate();
+		}
+
+		return count;
+	}
+
+}
---
+++ Amaats/src/main/java/servlet/ReviewServlet.java
@@ -0,0 +1,74 @@
+package servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.dao.ReviewDAO;
+import model.entity.ReviewBean;
+
+/**
+ * 口コミ機能の登録制御
+ */
+@WebServlet("/review-servlet")
+public class ReviewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ReviewServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// リクエストオブジェクトのエンコーディング方式の指定
+		request.setCharacterEncoding("UTF-8");
+
+		// リクエストパラメータの取得
+		int review_id =Integer.parseInt(request.getParameter("review_id"));
+		String review_text = request.getParameter("review_text");
+		String user_id = request.getParameter("user_id");
+		int sweets_id = Integer.parseInt(request.getParameter("sweets_id"));
+		
+		
+		ReviewBean review = new ReviewBean();
+		
+		review.setReview_id(review_id);
+		review.setReview_text(review_text);
+		review.setUser_id(user_id);
+		review.setSweets_id(sweets_id);
+		// DAOの生成
+		ReviewDAO dao = new ReviewDAO();
+
+		int count = 0;	// 処理件数
+
+		try {
+			// DAOの利用
+			count = dao.insertReview(review);
+
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+
+		// リクエストスコープへの属性の設定
+		request.setAttribute("count", count);
+		request.setAttribute("review_text", review_text);
+
+		// リクエストの転送
+		RequestDispatcher rd = request.getRequestDispatcher("sweetsInfo.jsp");
+		rd.forward(request, response);
+
+	}
+
+}
